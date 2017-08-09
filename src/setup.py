87f46a35eb55f342c85f73c0b648ed039bfc13d8
karltayeb <karl.tayeb@gmail.com>
2017-08-08 22:50:36 -0400

    to compile cython

---
+++ src/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Build import cythonize
+import numpy
+
+include_path = [numpy.get_include()]
+
+extensions = [
+    Extension('mycanalysis.src.ldsinference', ['ldsinference.pyx'],
+              include_dirs=[numpy.get_include()]),
+    Extension('mycanalysis.src.ldsmixmethods', ['ldsmixmethods.pyx'],
+              include_dirs=[numpy.get_include()])
+]
+
+setup(
+  ext_modules=cythonize(extensions, gdb_debug=True)
+)
